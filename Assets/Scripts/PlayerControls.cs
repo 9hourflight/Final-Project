b50d0bdff2849142df15ca6f36d2786899145a7b
VicenteDLR <vicente.delarosa1@g.austincc.edu>
2025-05-13 18:15:13 -0500

    player controller
    got player moving, need to do animations, jump, spit, etc

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -3,23 +3,35 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     private Rigidbody playerRigidBody;
+    private CharacterController controller;
+    private Camera playerCamera;
     [SerializeField]
     float speed;
     float horizontalInput;
     float verticalInput;
+    [SerializeField]
+    private float turnSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+        //controller = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
        
         transform.Translate((Vector3.forward * speed) * Time.deltaTime * verticalInput);
         transform.Translate((Vector3.right * speed) * Time.deltaTime * horizontalInput);
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput);
+        playerCamera.transform.Rotate( -mouseY, 0,  0);
+        transform.Rotate(0, mouseX, 0);
+        //transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * horizontalInput);
     }
 }
